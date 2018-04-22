8f27f6b3cf26fb9207beb4223871cdc9667afc15
pravat2016 <pravat@pulpstrategy.com>
2018-04-23 00:54:56 +0530

    url changes

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 $config['base_url'] = 'http://localhost/short/';
-$config['link_length'] = 8;
+$config['link_length'] = 6;
 /*
 |--------------------------------------------------------------------------
 | Index File
